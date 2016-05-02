17616fadd2bf9952838b0417ad2dd2bdb582c92f
ikegami <ikegamint02@gmail.com>
2016-05-02 10:35:22 +0900

    プロジェクト作成

---
+++ src/test/java/test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by ut on 2016/05/02.
+ */
+public class test {
+    @Test
+    public final void test(){
+        GravityCalculator g = new GravityCalculator();
+        g.main(new String[]{});
+    }
+}
