4e5f893154cddd406fc6b78091b76d18f3e1757a
ikegami <ikegamint02@gmail.com>
2016-05-02 10:59:35 +0900

    Assignment 1 Part Twoプログラム追加

--- src/main/java/GravityCalculator.java
+++ src/main/java/GravityCalculator.java
@@ -1,6 +1,3 @@
-/**
- * Created by ut on 2016/05/02.
- */
 public class GravityCalculator {
     public static void main(String[] arguments) {
         double gravity = -9.81; // Earth's gravity in m/s^2
@@ -10,5 +7,9 @@ public class GravityCalculator {
         double finalPosition = 0.0;
         System.out.println("The object's position after " + fallingTime +
                 " seconds is " + finalPosition + " m.");
+        System.out.println("Part Two: " + calcPosition(gravity, fallingTime, initialVelocity, initialPosition) + " m.");
+    }
+    private static double calcPosition(double acceleration, double time, double initial_velocity, double initial_position) {
+        return 0.5 * acceleration * time * time + initial_velocity * time+initial_position;
     }
 }
